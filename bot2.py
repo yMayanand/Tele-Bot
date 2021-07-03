4876b134b9aa727a76f470edaeecae506ad45f74
thinking Machine <libraryprojectx@gmail.com>
2021-07-03 19:58:40 +0530

    second commit

---
+++ bot2.py
@@ -0,0 +1,109 @@
+import logging
+import requests
+from bs4 import BeautifulSoup as bs
+import schedule
+import datetime
+import re
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+URL = "https://www.goodreturns.in/gold-rates/mumbai.html"
+
+
+
+
+# function for fetching gold price 
+def get_gold_price(update, context):
+    data = requests.get(URL)
+    parsed_data = bs(data.content, "html.parser")
+    gold_price = parsed_data.find_all(
+            "tr", class_="odd_row")
+    value = gold_price[1].find_all("td")[1].text.strip()
+    update.message.reply_text(value)
+    
+
+logger = logging.getLogger(__name__)
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def doggie(update, context):
+    print("reached bop")
+    url = get_image_url()
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo = url)
+
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1890665557:AAF3dJQlpoYUuw4WSocqS81uXqYex9HjPoE", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    
+
+    
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("doggie", doggie))
+    dp.add_handler(CommandHandler("show", get_gold_price))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
